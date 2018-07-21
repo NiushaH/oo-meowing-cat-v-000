cc1cacc91530499787faf183766e87452a06e719
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-21 05:05:42 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,17 @@
 Class Cat 
 attr_accessor :name 
 
-def name=(Cat_name)
-  @name = Cat_name
-end 
-
-def name(Cat_name)
-  @name
-end 
-
-
-def meow
-  puts "Meow!"
-end 
+  def name=(Cat_name)
+    @name = Cat_name
+  end 
+  
+  def name(Cat_name)
+    @name
+  end 
+  
+  
+  def meow
+    puts "Meow!"
+  end 
 
 end
