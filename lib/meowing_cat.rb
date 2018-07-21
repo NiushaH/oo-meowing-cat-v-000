49f5e7ce7463cb9714d3d64a8acf17ad559d14c8
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-21 05:13:14 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -6,11 +6,11 @@ attr_accessor :name
   end 
 
 
-  def name=(Cat_name)
-    @name = Cat_name
+  def name=(name)
+    @name = name
   end 
   
-  def name(Cat_name)
+  def name(name)
     @name
   end 
   
